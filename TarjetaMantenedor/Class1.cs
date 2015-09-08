1d69424602e5f97d2c5807152e4f3041735d57b1
dlorcav <diego.lorca.vasquez@gmail.com>
2015-09-08 13:37:05 -0300

    se complementa función para dejar grupo de acceso único

--- TarjetaMantenedor/Class1.cs
+++ TarjetaMantenedor/Class1.cs
@@ -200,6 +200,11 @@ namespace WindowsFormsApplication1
             string sard = " bien";
             try
             {
+                String consulta1 = "delete from TB_ACCESSGROUP_USER where nAgentIdn = '" + idusuario + "';";
+                SqlCommand sqd1 = new SqlCommand(consulta1, con1._SqlConnetion);
+                con1._SqlConnetion.Open();
+                sqd1.ExecuteNonQuery();
+                con1._SqlConnetion.Close();
                 String consulta = "Insert into TB_ACCESSGROUP_USER(nAccessIdn,nType,nAgentIdn) values('" + idgrupo + "','1000','" + idusuario + "');";
                 SqlCommand sqd = new SqlCommand(consulta, con1._SqlConnetion);
                 con1._SqlConnetion.Open();
